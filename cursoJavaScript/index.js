41027d57466d72289cc56e9c489c30273754271a
OsmarWilliam <osmarwilliam4@gmail.com>
2025-01-14 16:00:04 -0300

    Adicionando conteúdo sobre modulos

---
+++ cursoJavaScript/index.js
@@ -0,0 +1,6 @@
+import soma from './modulo.js';
+import { subtracao } from './modulo.js';
+
+soma(2,5); // 7
+
+subtracao(5,2); // 3
---
+++ cursoJavaScript/modulo.js
@@ -0,0 +1,7 @@
+export default function somar(a, b) {
+    return a + b;
+  }
+
+export function subtracao(a, b) {
+    return a - b;
+  }
