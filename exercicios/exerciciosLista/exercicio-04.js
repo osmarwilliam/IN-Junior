f2c4e53794fc1e95b333641913cae59361d50471
OsmarWilliam <osmarwilliam4@gmail.com>
2025-01-14 17:17:33 -0300

    Adicionando desafio 04

---
+++ exercicios/exerciciosLista/exercicio-04.js
@@ -0,0 +1,6 @@
+let real = parseFloat(prompt('Digite o valor em real: '));
+
+let dolar = real * 5.70;
+let euro = real * 6.10;
+
+alert(`R$${real} reais é o equivalente a $${dolar} dólares e €${euro} euros`); 
