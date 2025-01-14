b36de7b8ef4c5f3b1692604a8f07ab5b845ebd40
OsmarWilliam <osmarwilliam4@gmail.com>
2025-01-14 17:39:14 -0300

    Adicionando desafio 05

---
+++ exercicios/exerciciosLista/exercicio-05.js
@@ -0,0 +1,12 @@
+let numero = parseInt(prompt("Digite um número: "));
+
+if (numero % 15 == 0) {
+    alert("FizzBuzz");
+}
+else if (numero % 3 == 0) {
+    alert("Fizz");
+}
+else if (numero % 5 == 0) {
+    alert("Buzz");
+}  
+else {}
